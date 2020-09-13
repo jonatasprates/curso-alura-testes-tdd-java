a7c089c33dec3331dbc4e9463362b39d99dd5206
Jonatas Prates <jprates@dxc.com>
2020-09-13 15:03:17 -0300

    Cuidando dos testes, utilizando after, before builder e outros

--- src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -4,74 +4,103 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.List;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 import br.com.caelum.leilao.dominio.Usuario;
 import br.com.caelum.leilao.servico.Avaliador;
+import br.com.caelum.leilao.servico.CriadorDeLeilao;
+
+/*
+ * Ao contrário do @Before, métodos anotados com @After são executados após a execução do método de teste.
+   Utilizamos métodos @After quando nossos testes consomem recursos que precisam ser finalizados. 
+   Exemplos podem ser testes que acessam banco de dados, abrem arquivos, abrem sockets, e etc.
+   
+   Métodos anotados com @BeforeClass são executados apenas uma vez, antes de todos os métodos de teste.
+   O método anotado com @AfterClass, por sua vez, é executado uma vez, após a execução do último método de teste da classe.
+ */
 
 public class TesteDoAvaliador {
+	
+	private Avaliador leiloeiro;
+	private Usuario joao;
+	private Usuario jose;
+	private Usuario maria;
+	
+	@Before // executa este método antes dos métodos de cada testes
+	public void criaAvaliador() {
+		this.leiloeiro = new Avaliador();
+		this.joao = new Usuario("João");
+		this.jose = new Usuario("José");
+		this.maria = new Usuario("Maria");
+	}
+	
+	@After
+	public void finaliza() {
+	  System.out.println("fim");
+	}
+	
+	@BeforeClass
+	public static void testandoBeforeClass() {
+	  System.out.println("before class");
+	}
 
-	@Test
-	public void deveEntenderLancesEmOrdemCrescente() {
-		// parte 1: cenario
-		Usuario joao = new Usuario("João");
-		Usuario jose = new Usuario("José");
-		Usuario maria = new Usuario("Maria");
-		
-		Leilao leilao = new Leilao("Playstation 3 novo");
-		leilao.propoe(new Lance(joao, 250.00));
-		leilao.propoe(new Lance(jose, 300.00));
-		leilao.propoe(new Lance(maria, 400.00));
-		
-		// parte 2: acao
-		Avaliador leiloeiro = new Avaliador();
-		leiloeiro.avalia(leilao);
-		
-		// parte 3: validacao
-		assertEquals(400.0, leiloeiro.getMaiorLance(), 0.00001); //0.00001 -> delta para resolver problemas de arredondamento double
-		assertEquals(250.0, leiloeiro.getMenorLance(), 0.00001);
-		
+	@AfterClass
+	public static void testandoAfterClass() {
+	  System.out.println("after class");
 	}
+
+    @Test
+    public void deveEntenderLancesEmOrdemCrescente() {
+
+        Leilao leilao = new CriadorDeLeilao()
+            .para("Playstation 3 Novo")
+            .lance(joao, 250)
+            .lance(jose, 300)
+            .lance(maria, 400)
+            .constroi();
+
+        leiloeiro.avalia(leilao);
+
+        assertEquals(400.0, leiloeiro.getMaiorLance(), 0.00001);
+        assertEquals(250.0, leiloeiro.getMenorLance(), 0.00001);
+    }
+
+    @Test
+    public void deveEntenderLeilaoComApenasUmLance() {
+        Leilao leilao = new CriadorDeLeilao()
+        .para("Playstation 3 Novo")
+        .lance(joao, 1000)
+        .constroi();
+
+        leiloeiro.avalia(leilao);
+
+        assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.00001);
+        assertEquals(1000.0, leiloeiro.getMenorLance(), 0.00001);
+    }
 	
 	@Test
-	public void deveEntenderLeilaoComApenasUmLance() {
-		Usuario joao = new Usuario("João");
-		Leilao leilao = new Leilao("Playstation 3 novo");
-		
-		leilao.propoe(new Lance(joao, 1000.0));
-		
-		Avaliador leiloeiro = new Avaliador();
-		leiloeiro.avalia(leilao);
-		
-		assertEquals(1000.0, leiloeiro.getMaiorLance(), 0.00001);
-		assertEquals(1000.0, leiloeiro.getMenorLance(), 0.00001);
-		
-	}
-	
-	@Test
-	public void deveEncontratOsTresMaioresLances() {
-		Usuario joao = new Usuario("João");
-		Usuario maria = new Usuario("Maria");
-		
-		Leilao leilao = new Leilao("Playstation 3 novo");
-		
-		leilao.propoe(new Lance(joao, 100.00));
-		leilao.propoe(new Lance(maria, 200.00));
-		leilao.propoe(new Lance(joao, 300.00));
-		leilao.propoe(new Lance(maria, 400.00));
-		
-		Avaliador leiloeiro = new Avaliador();
-		leiloeiro.avalia(leilao);
-		
-		List<Lance> maiores = leiloeiro.getTresMaiores();
-		assertEquals(3, maiores.size());
-		assertEquals(400.0, maiores.get(0).getValor(), 0.00001);
-		assertEquals(300.0, maiores.get(1).getValor(), 0.00001);
-		assertEquals(200.0, maiores.get(2).getValor(), 0.00001);
-		
-		
-	}
+    public void deveEncontrarOsTresMaioresLances() {
+        Leilao leilao = new CriadorDeLeilao()
+            .para("Playstation 3 Novo")
+            .lance(joao, 100)
+            .lance(maria, 200)
+            .lance(joao, 300)
+            .lance(maria, 400)
+            .constroi();
+
+        leiloeiro.avalia(leilao);
+
+        List<Lance> maiores = leiloeiro.getTresMaiores();
+        assertEquals(3, maiores.size());
+        assertEquals(400.0, maiores.get(0).getValor(), 0.00001);
+        assertEquals(300.0, maiores.get(1).getValor(), 0.00001);
+        assertEquals(200.0, maiores.get(2).getValor(), 0.00001);
+    }
 	
 }
