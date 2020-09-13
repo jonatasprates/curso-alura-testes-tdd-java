f79cf6c5c22041b4823973be760cc07107a19c34
Jonatas Prates <jprates@dxc.com>
2020-09-13 09:42:30 -0300

    Começando com testes de unidade

---
+++ src/br/com/caelum/leilao/teste/TesteDoAvaliador.java
@@ -0,0 +1,37 @@
+package br.com.caelum.leilao.teste;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import br.com.caelum.leilao.dominio.Lance;
+import br.com.caelum.leilao.dominio.Leilao;
+import br.com.caelum.leilao.dominio.Usuario;
+import br.com.caelum.leilao.servico.Avaliador;
+
+public class TesteDoAvaliador {
+
+	@Test
+	public void deveEntenderLancesEmOrdemCrescente() {
+		// parte 1: cenario
+		Usuario joao = new Usuario("João");
+		Usuario jose = new Usuario("José");
+		Usuario maria = new Usuario("Maria");
+		
+		Leilao leilao = new Leilao("Playstation 3 novo");
+		leilao.propoe(new Lance(joao, 250.00));
+		leilao.propoe(new Lance(jose, 300.00));
+		leilao.propoe(new Lance(maria, 400.00));
+		
+		// parte 2: acao
+		Avaliador leiloeiro = new Avaliador();
+		leiloeiro.avalia(leilao);
+		
+		// parte 3: validacao
+		double maiorEsperado = 400;
+		double menorEsperado = 250;
+		
+		Assert.assertEquals(maiorEsperado, leiloeiro.getMaiorLance(), 0.00001); //0.00001 -> delta para resolver problemas de arredondamento double
+		Assert.assertEquals(menorEsperado, leiloeiro.getMenorLance(), 0.00001);
+		
+	}
+}
